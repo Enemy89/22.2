fc190012b633e60ef9b25ae9107dba1046a4497d
Enemy89 <66246104+Enemy89@users.noreply.github.com>
2023-09-20 21:51:32 +0300

    Add files via upload

--- untitled14/cmake-build-debug/cmake_install.cmake
+++ untitled14/cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/Rhan/CLionProjects/untitled14
+# Install script for directory: C:/Users/Rhan/CLionProjects/untitled16
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/untitled14")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/untitled16")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Rhan/CLionProjects/untitled14/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Rhan/CLionProjects/untitled16/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- untitled14/main.cpp
+++ untitled14/main.cpp
@@ -3,18 +3,28 @@
 #include <map>
 
 int main() {
-    std::string surnmw;
-    std::map <std::string, int> reg;
-    int i=0;
-    while (surnmw!="1") {
-        std::cin>>surnmw;
-        if (surnmw!="Next") {
-            std::pair<std::string, int> regPair(surnmw,i);
+    std::string surnme;
+    std::map<std::string, int> reg;
+    int i = 0;
+    while (surnme != "1") {
+        std::cin >> surnme;
+        if (surnme != "Next") {
+            std::map<std::string, int>::iterator itf = reg.find(surnme);
+            if (itf->first == surnme) {
+                itf->second += 1;
+                std::cout << itf->second << std::endl;
+            }
+            std::pair<std::string, int> regPair(surnme, i);
             reg.insert(regPair);
         } else {
             std::map<std::string, int>::iterator it = reg.begin();
-            std::cout<<it->first<<std::endl;
-            reg.erase(it->first);
+            std::cout << it->first << std::endl;
+            if (it->second == 0) {
+                reg.erase(it->first);
+            } else {
+                it->second -= 1;
+                std::cout << it->second << std::endl;
+            }
         }
     }
 }
